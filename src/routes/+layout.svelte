b6d1e845ff226c352b742d4e9ba9bb83f1f6d501
Johnny JTH <johanohly@gmail.com>
2024-03-06 23:18:59 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import '../app.pcss';
+
+  let title = 'Copenhagen Open 2024';
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+</svelte:head>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Button } from '$lib/components/ui/button';
+</script>
+
+<div
+  id="hero"
+  class="h-screen w-screen bg-[url('images/EO22_FinalRound.jpg')] bg-cover bg-center bg-no-repeat"
+>
+  <div class="relative h-full p-16 pt-8 text-center">
+    <h1 class="text-6xl font-bold text-primary lg:text-8xl">Copenhagen Open 2024</h1>
+    <p class="mt-2 text-xl text-background lg:text-3xl">Placeholder...</p>
+    <div class="flex flex-col h-[80vh] gap-8 mt-8 lg:flex-row">
+      <Button variant="huge" size="huge">Player</Button>
+      <Button variant="huge" size="huge">Tilskuer</Button>
+    </div>
+  </div>
+</div>
