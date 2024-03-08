9c686c2346bdd1276b1a5f785c9712d530a1d41d
Johnny JTH <johanohly@gmail.com>
2024-03-08 09:14:44 +0100

    SiteHeader fixes

--- src/lib/components/SiteHeader.svelte
+++ src/lib/components/SiteHeader.svelte
@@ -49,7 +49,7 @@
     .pathname !== '/' && 'bg-popover/75'} backdrop-blur-lg"
 >
   <div class="flex items-center w-full h-full page-container">
-    <div class="flex justify-start {$page.url.pathname === '/' ? 'max-md:flex-1' : 'flex-1'}">
+    <div class="flex justify-start {$page.url.pathname === '/' ? 'max-lg:flex-1' : 'flex-1'}">
       <a href="/">
         <img
           src="/images/edgpt-co.png"
@@ -81,13 +81,13 @@
               <Menu />
             </Drawer.Trigger>
             <Drawer.Portal>
-              <Drawer.Close bind:el={drawerClose} />
               <Drawer.Overlay class="fixed inset-0 bg-black/40" />
               <Drawer.Content
                 class="fixed bottom-0 left-0 right-0 mt-24 flex h-full max-h-[90%] flex-col rounded-t-[10px]"
               >
+                <Drawer.Close bind:el={drawerClose} />
                 <div class="flex-1 rounded-t-[10px] bg-background p-4">
-                  <div class="mx-auto sm:mb-8 h-1.5 w-12 flex-shrink-0 rounded-full bg-accent" />
+                  <div class="mx-auto h-1.5 w-12 flex-shrink-0 rounded-full bg-accent sm:mb-8" />
                   <div class="flex flex-col gap-4 p-8 sm:gap-8">
                     {#each NAV_ITEMS as item}
                       <a
@@ -110,7 +110,7 @@
                               on:click={() => drawerClose.click()}
                               href={item.href}
                               class={cn(
-                                'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors bg-accent/60 hover:bg-accent/40 hover:text-primary',
+                                'block select-none space-y-1 rounded-md bg-accent/60 p-3 leading-none no-underline outline-none transition-colors hover:bg-accent/40 hover:text-primary',
                                 $page.url.pathname === item.href && 'bg-accent/60 text-primary'
                               )}
                             >
@@ -132,12 +132,12 @@
       </div>
     {:else}
       <!-- Hovedmenuen er lidt anderledes på startsiden -->
-      <div class="justify-center hidden md:flex md:flex-1">
+      <div class="justify-center hidden lg:flex lg:flex-1">
         <h1 class="text-2xl font-bold unstyled text-primary">DGPT Europe - Copenhagen Open 2024</h1>
       </div>
-      <div class="flex justify-end max-md:flex max-md:flex-col">
-        <h1 class="text-xl font-bold text-primary sm:text-2xl md:hidden">Copenhagen Open 2024</h1>
-        <div class="flex gap-2 max-md:justify-end">
+      <div class="flex justify-end max-lg:flex max-lg:flex-col">
+        <h1 class="text-xl font-bold unstyled text-primary sm:text-2xl lg:hidden">Copenhagen Open 2024</h1>
+        <div class="flex gap-2 max-lg:justify-end">
           <CalendarDays />
           <span class="text-sm md:text-md sm:text-base">May 10-12, 2024</span>
         </div>
