5f818c3eef055a06781c05bab5bf5783a074fee3
Johnny JTH <johanohly@gmail.com>
2024-03-08 09:15:25 +0100

    Fix front page headers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,11 +12,11 @@
     <div class="flex flex-col flex-auto gap-8 lg:flex-row">
       <Button href="/publikum" variant="huge" size="huge">
         <Users strokeWidth="3" class="w-12 h-12 text-primary lg:h-24 lg:w-24" />
-        <h2 class="text-3xl lg:text-5xl text-outline text-primary">For publikum</h2>
+        <h2 class="text-3xl unstyled lg:text-5xl text-primary">For publikum</h2>
       </Button>
       <Button href="/player" variant="huge" size="huge">
         <User strokeWidth="3" class="w-12 h-12 text-primary lg:h-24 lg:w-24" />
-        <h2 class="text-3xl lg:text-5xl text-outline text-primary">For players</h2>
+        <h2 class="text-3xl unstyled lg:text-5xl text-primary">For players</h2>
       </Button>
     </div>
   </div>
