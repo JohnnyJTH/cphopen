90bb2b0a83d41f5e00bd495638a3a9bb97c39a2a
Johnny JTH <johanohly@gmail.com>
2024-03-09 23:56:38 +0100

    Logoet gik langt over header, enten skal headeren lavet større eller logoet bliver meget utydeligt

--- src/lib/components/SiteHeader.svelte
+++ src/lib/components/SiteHeader.svelte
@@ -125,7 +125,6 @@
           src="/images/edgpt-co.png"
           class="w-12 h-12"
           alt="DGPT Europe - Copenhagen Open 2024"
-          style="position: fixed; height: 70px !important; width: 70px !important; margin-top: -1rem !important;"
         />
       </a>
     </div>
