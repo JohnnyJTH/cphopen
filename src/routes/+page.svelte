76e2a7689fd074d18a2cfc6e094231bc93f91745
Johnny JTH <johanohly@gmail.com>
2024-03-10 13:32:05 +0100

    Simpelt logo på forsiden

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,15 +8,20 @@
   style="background-image: url(images/co2024-website_background.jpg)"
   class="-mb-16 -mt-16 h-screen w-screen bg-[''] bg-cover bg-center bg-no-repeat"
 >
+  <div
+    class="absolute z-10 -translate-x-1/2 -translate-y-1/2 rounded-full left-1/2 top-1/2 bg-primary"
+  >
+    <img src="/images/edgpt-co.png" class="p-1 w-96 lg:w-64 xl:w-80 2xl:w-96" />
+  </div>
   <div class="relative flex flex-col h-full px-8 py-24 text-center md:px-16 md:py-32">
     <div class="flex flex-col flex-auto gap-8 lg:flex-row">
       <Button href="/publikum" variant="huge" size="huge">
-        <Users strokeWidth="3" class="w-12 h-12 text-primary text-white lg:h-24 lg:w-24" />
-        <h2 class="text-3xl unstyled lg:text-5xl text-primary text-white">For publikum</h2>
+        <Users strokeWidth="3" class="w-12 h-12 text-primary lg:h-24 lg:w-24" />
+        <h2 class="text-3xl text-white unstyled lg:text-5xl">For publikum</h2>
       </Button>
       <Button href="/player" variant="huge" size="huge">
         <User strokeWidth="3" class="w-12 h-12 text-primary lg:h-24 lg:w-24" />
-        <h2 class="text-3xl unstyled lg:text-5xl text-primary">For players</h2>
+        <h2 class="text-3xl unstyled text-primary lg:text-5xl">For players</h2>
       </Button>
     </div>
   </div>
