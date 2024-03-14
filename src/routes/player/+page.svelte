130f67c8b52fdaeb35bb8b4af727558938600155
SinusFrank <162629265+SinusFrank@users.noreply.github.com>
2024-03-15 00:04:31 +0100

    SF

--- src/routes/player/+page.svelte
+++ src/routes/player/+page.svelte
@@ -50,8 +50,6 @@
       In the top menu you can find information regarding schedule, course layout and practical
       information surrounding the tournament about accommodation, food etc.<br />
       This information will be published continuously, when they are set.
-      In the top menu you can find information regarding schedule, course layout and practical information surrounding the tournament about accommodation, food etc.<br>
-      This information will be published continuously, when it is set.
     </p>
     <p>We are delighted to be hosting you and the rest of the field!</p>
   </div>
