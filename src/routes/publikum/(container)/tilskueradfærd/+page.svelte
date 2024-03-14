aea4a7e4500c3296a81368cc432706d4e3dc13ee
SinusFrank <162629265+SinusFrank@users.noreply.github.com>
2024-03-14 12:33:16 +0100

    SF

--- src/routes/publikum/(container)/tilskueradfærd/+page.svelte
+++ src/routes/publikum/(container)/tilskueradfærd/+page.svelte
@@ -1,7 +1,11 @@
 <h1>Tilskueradfærd</h1>
 <p>
-  Ligesom i traditionel golf er spillerne meget fokuserede under spillet, så vi beder indtrængende
-  om, at man holder sig i ro, mens der kastes/puttes og sørger for at have sin mobiltelefon på
-  lydløs. Vi opfordrer alle til at samles langs hullerne for at komme så tæt på spillet som muligt,
-  men samtidig beder vi om, at man ikke krydser banen, mens der er spil i gang.
+Tilskuere må ikke nærme sig spillerne. Dette gælder for alle områder i parken: Parkeringsområdet, opvarmningsområdet og banen. 
+Autografsessioner vil blive arrangeret, efter runderne er afsluttet. 
+</p>
+<p>
+Ligesom i traditionel golf er spillerne meget fokuserede under spillet, så vi beder indtrængende om, at man holder sig i ro, mens der kastes/puttes og sørger for at have sin mobiltelefon på lydløs. Vi opfordrer alle til at samles langs hullerne for at komme så tæt på spillet som muligt, men samtidig beder vi om, at man ikke krydser banen, mens der er spil i gang.
+</p>
+<p>
+Vi henviser til, at parkering foregår uden for selve parken og/eller man ankommer til fods, da der er begrænsede antal P-pladser – og der derfor højst sandsynligt alligevel er fyldt op.
 </p>
