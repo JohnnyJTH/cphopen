6a4978de1d72e35f255c078650a7c316aad2fd24
Johnny JTH <johanohly@gmail.com>
2024-03-15 09:23:31 +0100

    Schedule day dates

--- src/routes/player/schedule/_components/Schedule.svelte
+++ src/routes/player/schedule/_components/Schedule.svelte
@@ -4,46 +4,67 @@
 
 <div class="grid max-lg:gap-8 lg:grid-cols-2 xl:grid-cols-3">
   <div class="space-y-4">
-    <h3 class="text-center">Monday</h3>
+    <div class="text-center">
+      <h3 class="mb-0">Monday</h3>
+      <p class="font-mono text-sm leading-none text-muted-foreground unstyled">May 6th</p>
+    </div>
     <div>
-      <ScheduleItem time="08:00" title="Course open for practice" last />
+      <ScheduleItem time="08:00" title="Course open for practice" />
       <ScheduleItem time="16:30" title="Qualification Tournament" last />
     </div>
   </div>
   <div class="space-y-4">
-    <h3 class="text-center">Tuesday</h3>
+    <div class="text-center">
+      <h3 class="mb-0">Tuesday</h3>
+      <p class="font-mono text-sm leading-none text-muted-foreground unstyled">May 7th</p>
+    </div>
     <div>
       <ScheduleItem time="08:00" title="Course open for practice" last />
     </div>
   </div>
   <div class="space-y-4">
-    <h3 class="text-center">Wednesday</h3>
+    <div class="text-center">
+      <h3 class="mb-0">Wednesday</h3>
+      <p class="font-mono text-sm leading-none text-muted-foreground unstyled">May 8th</p>
+    </div>
     <div>
       <ScheduleItem time="08:00" title="Course open for practice" last />
     </div>
   </div>
   <div class="space-y-4">
-    <h3 class="text-center">Thursday</h3>
+    <div class="text-center">
+      <h3 class="mb-0">Thursday</h3>
+      <p class="font-mono text-sm leading-none text-muted-foreground unstyled">May 9th</p>
+    </div>
     <div>
       <ScheduleItem time="08:00" title="Course open for practice" last />
     </div>
   </div>
   <div class="space-y-4">
-    <h3 class="text-center">Friday</h3>
+    <div class="text-center">
+      <h3 class="mb-0">Friday</h3>
+      <p class="font-mono text-sm leading-none text-muted-foreground unstyled">May 10th</p>
+    </div>
     <div>
       <ScheduleItem time="08:30" title="First Tee-Off" important />
       <ScheduleItem time="16:00" title="Last Tee-Off" last />
     </div>
   </div>
   <div class="space-y-4">
-    <h3 class="text-center">Saturday</h3>
+    <div class="text-center">
+      <h3 class="mb-0">Saturday</h3>
+      <p class="font-mono text-sm leading-none text-muted-foreground unstyled">May 11th</p>
+    </div>
     <div>
       <ScheduleItem time="08:30" title="First Tee-Off" />
       <ScheduleItem time="16:00" title="Last Tee-Off" last />
     </div>
   </div>
   <div class="space-y-4">
-    <h3 class="text-center">Sunday</h3>
+    <div class="text-center">
+      <h3 class="mb-0">Sunday</h3>
+      <p class="font-mono text-sm leading-none text-muted-foreground unstyled">May 12th</p>
+    </div>
     <div>
       <ScheduleItem time="08:30" title="First Tee-Off" />
       <ScheduleItem time="16:00" title="Last Tee-Off" last />
--- src/routes/player/schedule/_components/ScheduleItem.svelte
+++ src/routes/player/schedule/_components/ScheduleItem.svelte
@@ -9,7 +9,7 @@
 <div class="flex flex-row items-stretch gap-4">
   <p class="w-24 text-sm text-right uppercase unstyled shrink-0 text-muted-foreground">{time}</p>
   <div class="block w-[1px] shrink-0 bg-muted-foreground"></div>
-  <div class="flex-1 space-y-4 {!last && 'pb-12'}">
+  <div class="flex-1 space-y-3 {!last && 'pb-12'}">
     <h4 class="text-xl font-bold {important && 'text-primary'}">{title}</h4>
     <p class={important ? 'text-white' : 'text-muted-foreground'}>
       {description}
