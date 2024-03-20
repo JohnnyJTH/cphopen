a0b732c9dc3cfdfeb2ac88e4c426815f8521c718
Johnny JTH <johanohly@gmail.com>
2024-03-20 21:54:45 +0100

    Add page content

--- src/routes/player/(container)/accomodation/+page.svelte
+++ src/routes/player/(container)/accomodation/+page.svelte
@@ -1,2 +1,12 @@
 <h1>Accomodation</h1>
-<p>This page is under construction</p>
+<p>
+  Copenhagen boasts a little shy of 25,000 hotel rooms from five-star luxury to hostels. On top of
+  that there are plenty of Airbnb's and couches to surf. In other words, it will be no problem to
+  find a place to crash.
+</p>
+<p>
+  In close proximity of the course at Valbyparken you'll find the cheap hostel “a&o Copenhagen
+  Sydhavn” and the pricier “Scandic Sydhavn”, “Scandic Sluseholmen” and “Moxy Sydhavnen”. They're
+  all a mere 5-minute bike ride from Tee 1. A little further away (15 minutes on bike) you can book
+  cheap rooms at “Cabinn Copenhagen” and “Wakeup Copenhagen”.
+</p>
--- src/routes/player/(container)/spotters/+page.svelte
+++ src/routes/player/(container)/spotters/+page.svelte
@@ -1,2 +1,7 @@
 <h1>Spotters</h1>
-<p>This page is under construction</p>
+<p>
+  Spotters DO NOT make rulings. They can however guide the group but their call does NOT supersede
+  the ruling of the group. Do remember that the spotters are volunteers helping out to give you the
+  best possible experience participating in the Copenhagen Open 2024 and a “Thank you” or a high
+  five goes a long way.
+</p>
