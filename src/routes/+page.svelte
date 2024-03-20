2b9b62cc68881ab3e79d62a550b8e720b2aeee3b
SinusFrank <162629265+SinusFrank@users.noreply.github.com>
2024-03-20 10:55:03 +0100

    SF

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,7 +17,7 @@
     <div class="flex flex-col flex-auto gap-8 lg:flex-row">
       <Button href="/publikum" variant="huge" size="huge">
         <Users strokeWidth="3" class="w-12 h-12 text-white lg:h-24 lg:w-24" />
-        <h2 class="text-3xl font-medium text-white unstyled lg:text-5xl">Info til publikum</h2>
+        <h2 class="text-3xl font-medium text-white unstyled lg:text-5xl">Til publikum</h2>
       </Button>
       <Button href="/player" variant="huge" size="huge">
         <User strokeWidth="3" class="w-12 h-12 text-primary lg:h-24 lg:w-24" />
