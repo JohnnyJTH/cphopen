7df820549c1547121aa599d0f839506cd8eec24d
SinusFrank <162629265+SinusFrank@users.noreply.github.com>
2024-04-27 08:12:35 +0200

    SF

--- src/routes/player/(container)/ties/+page.svelte
+++ src/routes/player/(container)/ties/+page.svelte
@@ -1,2 +1,2 @@
 <h1>Ties</h1>
-<p>Ties for first places in both MPO and FPO are resolved by sudden death on holes 7, 8 and 18.</p>
+<p>Ties for first places in both MPO and FPO are resolved by sudden death on holes 7, 15 and 18.</p>
