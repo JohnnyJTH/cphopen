0f39f773acd2f60a7e5e661a317e2208c2f63539
SinusFrank <162629265+SinusFrank@users.noreply.github.com>
2024-04-30 07:54:53 +0200

    SF

--- src/routes/player/schedule/_components/Schedule.svelte
+++ src/routes/player/schedule/_components/Schedule.svelte
@@ -5,48 +5,48 @@
 <div class="grid max-lg:gap-8 lg:grid-cols-2 xl:grid-cols-3">
   <div class="space-y-4">
     <div class="text-center">
-      <h3 class="mb-0">Monday</h3>
+      <h3 class="mb-0 text-primary">Monday</h3>
       <p class="font-mono text-sm leading-none text-muted-foreground unstyled">May 6th</p>
     </div>
     <div>
-      <ScheduleItem time="09:00-16:00" title="Course open for practice" description="PRACTICE IS ONLY FOR PLAYERS WITH A SPOT IN THE MAIN TOURNAMENT OR THE QUALIFICATION TOURNAMENT!" />
+      <ScheduleItem time="09:00-16:00" title="Course open for practice" description="PRACTICE ONLY FOR MAIN/QUALIFICATION TOURNAMENT PLAYERS!" />
       <ScheduleItem time="16:30" title="Qualification Tournament" description="The qualification tournament is for registered players for this specific event. No other players are allowed to use the course from 16:30 on Monday!" last />
     </div>
   </div>
   <div class="space-y-4">
     <div class="text-center">
-      <h3 class="mb-0">Tuesday</h3>
+      <h3 class="mb-0 text-primary">Tuesday</h3>
       <p class="font-mono text-sm leading-none text-muted-foreground unstyled">May 7th</p>
     </div>
     <div>
-      <ScheduleItem time="09:00" title="Course open for practice" description="PRACTICE IS ONLY FOR PLAYERS WITH A SPOT IN THE MAIN TOURNAMENT! Register here for tee-time: https://tinyurl.com/co24-pr-tu" />
+      <ScheduleItem time="09:00" title="Course open for practice" description="PRACTICE ONLY FOR MAIN TOURNAMENT PLAYERS! Register here for tee-time: https://tinyurl.com/co24-pr-tu" />
       <ScheduleItem time="17:00-18:00" title="Clinic with Paul Ulibarri" description="Privately arranged clinic - register here: https://tinyurl.com/co24-uli" last />
     </div>
   </div>
   <div class="space-y-4">
     <div class="text-center">
-      <h3 class="mb-0">Wednesday</h3>
+      <h3 class="mb-0 text-primary">Wednesday</h3>
       <p class="font-mono text-sm leading-none text-muted-foreground unstyled">May 8th</p>
     </div>
     <div>
-      <ScheduleItem time="09:00" title="Course open for practice" description="PRACTICE IS ONLY FOR PLAYERS WITH A SPOT IN THE MAIN TOURNAMENT! Register here for tee-time: https://tinyurl.com/co24-pr-we" />
+      <ScheduleItem time="09:00" title="Course open for practice" description="PRACTICE ONLY FOR MAIN TOURNAMENT PLAYERS! Register here for tee-time: https://tinyurl.com/co24-pr-we" />
       <ScheduleItem time="09:00-18:00" title="Course assets are set up" description="Course assets include circle 1 and 2 whiskers, tents and beach flags." last />
     </div>
   </div>
   <div class="space-y-4">
     <div class="text-center">
-      <h3 class="mb-0">Thursday</h3>
+      <h3 class="mb-0 text-primary">Thursday</h3>
       <p class="font-mono text-sm leading-none text-muted-foreground unstyled">May 9th</p>
     </div>
     <div>
-      <ScheduleItem time="09:00" title="Course open for practice" description="PRACTICE IS ONLY FOR PLAYERS WITH A SPOT IN THE MAIN TOURNAMENT! Register here for tee-time: https://tinyurl.com/co24-pr-th" />
+      <ScheduleItem time="09:00" title="Course open for practice" description="PRACTICE ONLY FOR MAIN TOURNAMENT PLAYERS! Register here for tee-time: https://tinyurl.com/co24-pr-th" />
       <ScheduleItem time="09:00-17:00" title="Practice area is set up" description="Includes fencing off some areas." />
       <ScheduleItem time="15:00-17:00" title="Press Conference" last />
     </div>
   </div>
   <div class="space-y-4">
     <div class="text-center">
-      <h3 class="mb-0">Friday</h3>
+      <h3 class="mb-0 text-primary">Friday</h3>
       <p class="font-mono text-sm leading-none text-muted-foreground unstyled">May 10th</p>
     </div>
     <div>
@@ -57,7 +57,7 @@
   </div>
   <div class="space-y-4">
     <div class="text-center">
-      <h3 class="mb-0">Saturday</h3>
+      <h3 class="mb-0 text-primary">Saturday</h3>
       <p class="font-mono text-sm leading-none text-muted-foreground unstyled">May 11th</p>
     </div>
     <div>
@@ -68,7 +68,7 @@
   </div>
   <div class="space-y-4">
     <div class="text-center">
-      <h3 class="mb-0">Sunday</h3>
+      <h3 class="mb-0 text-primary">Sunday</h3>
       <p class="font-mono text-sm leading-none text-muted-foreground unstyled">May 12th</p>
     </div>
     <div>
