fc2cbfc59e4c94d88106fb4d4f5931dd95d3ff5c
SinusFrank <162629265+SinusFrank@users.noreply.github.com>
2024-05-02 14:11:22 +0200

    SF

--- src/routes/player/(container)/course/+page.svelte
+++ src/routes/player/(container)/course/+page.svelte
@@ -4,9 +4,10 @@
     <i>Note: The paving stones between the grass and the road on the right side of hole 14 - left side of holes 12 / 13 - are NOT OB; only the road itself (and beyond) is OB!</i><br /><br />
     Note also that there are mandatories on holes 1, 9, 11 and 14. There will be clearly marked arrows ony the respective trees, clearly marked mandatory lines on the grass and marked drop zones for missed mandatories.
 </p>
-<h4>Here you can see all the holes one by one:</h4>
+<h3>Here you can see all the holes one by one:</h3>
 <div class="columns-3">
-    <img src="images/teesigns/TeeSignsCopenhagen2024-1.png" />
-    <img src="images/teesigns/TeeSignsCopenhagen2024-2.png" />
-    <img src="images/teesigns/TeeSignsCopenhagen2024-3.png" />
+    <a href="/images/teesigns/TeeSignsCopenhagen2024-1.png" target="_blank"><img src="/images/teesigns/TeeSignsCopenhagen2024-1.png" /></a>
+    <a href="/images/teesigns/TeeSignsCopenhagen2024-2.png" target="_blank"><img src="/images/teesigns/TeeSignsCopenhagen2024-2.png" /></a>
+    <a href="/images/teesigns/TeeSignsCopenhagen2024-3.png" target="_blank"><img src="/images/teesigns/TeeSignsCopenhagen2024-3.png" /></a>
+    <a href="/images/teesigns/TeeSignsCopenhagen2024-4.png" target="_blank"><img src="/images/teesigns/TeeSignsCopenhagen2024-4.png" /></a>
 </div>
