269ad6262e58d0a63c155c79edb7bfc17dc501ec
SinusFrank <162629265+SinusFrank@users.noreply.github.com>
2024-05-02 14:05:14 +0200

    SF

--- src/routes/publikum/(container)/kort/+page.svelte
+++ src/routes/publikum/(container)/kort/+page.svelte
@@ -1,3 +1,3 @@
 <h1>Områdekort</h1>
 <p>På nedenstående kort findes information om turneringsområdet, herunder teltene ved turneringscentrum - og selve hullerne på banen. Kortet kan åbnes i fuld skærm</p>
-<iframe src="https://www.google.com/maps/d/embed?mid=1A_yTB7tFt0gkMR7zfW7QCu70UVhZND0&ehbc=2E312F&noprof=1" class="w-full h-48"></iframe>
+<iframe src="https://www.google.com/maps/d/embed?mid=1A_yTB7tFt0gkMR7zfW7QCu70UVhZND0&ehbc=2E312F&noprof=1" class="w-full h-96"></iframe>
