7225c93e86e52f4a7a3534935636b062fc8083ee
SinusFrank <162629265+SinusFrank@users.noreply.github.com>
2024-05-02 12:37:05 +0200

    SF

--- src/routes/player/(container)/course/+page.svelte
+++ src/routes/player/(container)/course/+page.svelte
@@ -1,2 +1,7 @@
 <h1>Course</h1>
-<p>This page is under construction</p>
+<p>The course is a varied and challenging course featuring both long, open and narrow holes, plus a few smaller hills and 2 elevated baskets. The par for the course is 64 with a total length of 2667 meters for the MPO division, and par 66 and 2466 meters for the FPO division.<br><BR>
+<i>Note: The paving stones between the grass and the road on the right side of hole 14 - left side of holes 12 / 13 - are NOT OB; only the road itself (and beyond) is OB!<BR><BR>
+Note also that there are mandatories on holes 1, 9, 11 and 14. There will be clearly marked arrows ony the respective trees, clearly marked mandatory lines on the grass and marked drop zones for missed mandatories.</p>
+<div class="columns-3">
+    <img src="https://drive.google.com/file/d/1A7NN43O9w0bGNYt71LPoIInw-XUcKNR_/view?usp=drive_link">
+</div>
