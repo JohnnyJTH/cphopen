00aad71abcfda74f3e02bb212016043ada1279d7
SinusFrank <162629265+SinusFrank@users.noreply.github.com>
2024-05-06 22:43:40 +0200

    SD

--- src/routes/player/(container)/course/+page.svelte
+++ src/routes/player/(container)/course/+page.svelte
@@ -2,8 +2,11 @@
 <p>
     The course is a varied and challenging course featuring both long, open and narrow holes, plus a few smaller hills and 2 elevated baskets. The par for the course is 64 with a total length of 2667 meters for the MPO division, and par 66 and 2466 meters for the FPO division.<br /><br />
     Note also that there are mandatories on holes 1, 9, 11 and 14. There will be clearly marked arrows ony the respective trees, clearly marked mandatory lines on the grass and marked drop zones for missed mandatories.
-</p>
     <i>Note: THIS YEAR the paving stones between the grass and the road on the right side of hole 14 - left side of holes 12 / 13 - <b>ARE OB</b>!</i><br /><br />
+</p>
+<p>The full course map is here - and can be downloaded:<br />
+    <a href="/images/CO24-Course-Map-v3.png" target="_blank"><img src="/images/CO24-Course-Map-v3.png" /></a>
+</p>
 <h3>Here you can see all the holes one by one:</h3>
 <div class="columns-3">
     <a href="/images/teesigns/TeeSignsCopenhagen2024-1.png" target="_blank"><img src="/images/teesigns/TeeSignsCopenhagen2024-1.png" /></a>
