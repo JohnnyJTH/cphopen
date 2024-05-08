cc0dc008df234c884ba6379b5b804af03a1456be
SinusFrank <162629265+SinusFrank@users.noreply.github.com>
2024-05-09 00:12:34 +0200

    SF

--- src/routes/player/(container)/prize/+page.svelte
+++ src/routes/player/(container)/prize/+page.svelte
@@ -1,2 +1,2 @@
 <h1>Prizes</h1>
-<p>This page is under construction</p>
+<p>The projected payout can be seen at the <a href="https://www.pdga.com/apps/tournament/manager/payouts_projected?TournID=78193" target="_blank">PDGA tournament page</a>.</p>
